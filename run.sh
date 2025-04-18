7c46771e48ed57d6208c69666fd8c05e04edaf7f
fabian <fbarulli@gmail.com>
2025-04-18 16:06:45 +0000

    semi-fixed

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-  docker-compose -f docker/compose/docker-compose.yml -f docker/compose/docker-compose.db.yml -f docker/compose/docker-compose.base.yml -f docker/compose/docker-compose.airflow.yml "$@"
+# Use 'docker compose' for V2+
+docker compose -f docker/compose/docker-compose.yml -f docker/compose/docker-compose.db.yml -f docker/compose/docker-compose.base.yml -f docker/compose/docker-compose.airflow.yml "$@"
